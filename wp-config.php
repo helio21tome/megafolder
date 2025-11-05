aad2238206977b26d1c1bc86b94ab68781d24d38
Helio Tome <132224640+helio21tome@users.noreply.github.com>
2025-11-05 02:30:38 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,10 +1,14 @@
 <?php
+// Lê variáveis do Render
 define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
 define('DB_USER', getenv('WORDPRESS_DB_USER'));
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 define('DB_HOST', getenv('WORDPRESS_DB_HOST') . ':5432');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
+
+// Força PostgreSQL
+define('DB_DRIVER', 'pdo_pgsql');
 
 $table_prefix = 'wp_';
 define('WP_DEBUG', false);
